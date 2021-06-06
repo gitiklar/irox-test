71016ee8842f76a6518637d89da2b56f19d06153
gitiklar <65737909+gitiklar@users.noreply.github.com>
2021-06-06 18:56:37 +0300

    update

--- src/redux/reducers/tableDataReducer.js
+++ src/redux/reducers/tableDataReducer.js
@@ -1,5 +1,5 @@
 import produce from 'immer';
-import { UPDATE_JSON_DATA , UPDATE_SELECTED_ROW, UPDATE_JSON_DATA_AFTER_ADDITIONALS } from '../actions';
+import { UPDATE_JSON_DATA , UPDATE_SELECTED_ROW } from '../actions';
 
 const initialState = {
     jsonData: [],
@@ -12,9 +12,6 @@ export default produce((state , action) => {
         case UPDATE_JSON_DATA:
             state.jsonData = action.payload.jsonData;
             state.summary = action.payload.summary;
-            break;
-        case UPDATE_JSON_DATA_AFTER_ADDITIONALS:
-            state.jsonData = action.payload;
             break;
         case UPDATE_SELECTED_ROW: 
             state.selectedRow = action.payload;
